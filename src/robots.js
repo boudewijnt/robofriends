1327dd06f8d60b460c6e1e82e7159c41acaf7baf
boudewijnt <info@timmers-interim.nl>
2022-03-23 13:50:06 +0100

    final product

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'Sincere@april.biz'
+	},
+	{
+		id: 2,
+		name: 'Boris Becker',
+		username: 'Borry',
+		email: 'Sincere@april.biz'
+	},
+	{
+		id: 3,
+		name: 'Jelle Timmers',
+		username: 'Jelle',
+		email: 'Sincere@april.biz'
+	},
+	{
+		id: 4,
+		name: 'Lucas Timmers',
+		username: 'Lucas',
+		email: 'Sincere@april.biz'
+	},
+	{
+		id: 5,
+		name: 'Astrid Timmers',
+		username: 'Astrid',
+		email: 'Sincere@april.biz'
+	},
+	{
+		id: 6,
+		name: 'Myra',
+		username: 'Myra',
+		email: 'Sincere@april.biz'
+	},
+	{
+		id: 7,
+		name: 'Tess',
+		username: 'Tess',
+		email: 'Sincere@april.biz'
+	},
+	{
+		id: 8,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'Sincere@april.biz'
+	},
+	{
+		id: 9,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'Sincere@april.biz'
+	},
+	{
+		id: 10,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'Sincere@april.biz'
+	},
+]
